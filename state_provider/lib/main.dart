1ae221972024717f39373466b56c7495a69e988d
faridrama123 <systemcalls915@gmail.com>
2021-06-06 21:39:04 +0700

    statemanagement dengan provider

---
+++ state_provider/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_provider/provider/done_module_provider.dart';
+import 'package:state_provider/ui/module_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => DoneModuleProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ModulePage(),
+      ),
+    );
+  }
+}
---
+++ state_provider/lib/ui/done_module_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_provider/provider/done_module_provider.dart';
+
+class DoneModuleList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final doneModuleList =
+        Provider.of<DoneModuleProvider>(context, listen: false).doneModuleList;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Done Module List'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(doneModuleList[index]),
+          );
+        },
+        itemCount: doneModuleList.length,
+      ),
+    );
+  }
+}
