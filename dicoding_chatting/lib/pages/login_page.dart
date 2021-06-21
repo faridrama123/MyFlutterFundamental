00d6231ee070f9713343a28b12c23a040dc65e65
faridrama123 <systemcalls915@gmail.com>
2021-06-21 19:16:06 +0700

    no message

---
+++ dicoding_chatting/lib/pages/login_page.dart
@@ -0,0 +1,114 @@
+import 'package:dicoding_chatting/pages/chat_page.dart';
+import 'package:dicoding_chatting/pages/register_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static const String id = 'login_page';
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _auth = FirebaseAuth.instance;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _obscureText = true;
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _isLoading
+                ? Center(child: CircularProgressIndicator())
+                : Container(),
+            Hero(
+              tag: 'Dicoding Chatting',
+              child: Text(
+                'Dicoding Chatting',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+            SizedBox(height: 24.0),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: _obscureText,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
+                hintText: 'Password',
+              ),
+            ),
+            SizedBox(height: 24.0),
+            MaterialButton(
+              child: Text('Login'),
+              color: Theme.of(context).primaryColor,
+              textTheme: ButtonTextTheme.primary,
+              height: 40,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              onPressed: _login,
+            ),
+            TextButton(
+              child: Text('Does not have an account yet? Register here'),
+              onPressed: () => Navigator.pushNamed(context, RegisterPage.id),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _login() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      final email = _emailController.text;
+      final password = _passwordController.text;
+
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      Navigator.pushReplacementNamed(context, ChatPage.id);
+    } catch (e) {
+      final snackbar = SnackBar(content: Text(e.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
